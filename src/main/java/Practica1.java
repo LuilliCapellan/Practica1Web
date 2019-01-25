52576f54565f5d083a9f3244e0883a7a235b9769
Luis Capellan <luillicapellan29@gmail.com>
2019-01-24 20:23:00 -0500

    Inicio de practica
    Practica creada ahora empiezo a trabajar

---
+++ src/main/java/Practica1.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Created by Luis Capellan on 24/01/2019
+ *
+ */
+
+
+
+public class Practica1 {
+    public static void main(String [] args){
+
+        System.out.println("Hello World");
+
+    }
+}
